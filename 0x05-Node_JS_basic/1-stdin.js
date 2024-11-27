8bdf6cca238bb5c51704c0e753674ef7a8fcf52d
Francis Waihiga <francisgwaihiga@gmail.com>
2024-11-27 05:20:34 +0300

    Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-if (process.stdin.isTTY) {
-  process.stdin.on('message', (message) => {
-    process.stdout.write(`Your name is: ${message.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('message', (message) => {
-    process.stdout.write(`Your name is: ${message.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+process.stdin.resume();
+
+process.stdin.on('data', (message) => {
+  process.stdout.write(`Your name is: ${message.toString().trim()}\n`);
+  process.exit();
+});
+
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
+});
